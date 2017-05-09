bb8c14f5b7796b9f903bae1ee08cb00734e4e22a
Andreas Wiman <andreas.wiman@arbetsformedlingen.se>
2017-05-09 16:51:33 +0200

    Sec

---
+++ Rymden/Assets/JSONDemo.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class JSONDemo : MonoBehaviour {
+
+	string path;
+	string jsonString;
+	// Use this for initialization
+	void Start () {
+
+		path = Application.streamingAssetsPath + "/100.json";
+		jsonString = File.ReadAllText (path);
+		Creature Yumo = JsonUtility.FromJson<Creature> (jsonString);
+		Debug.Log (Yumo.kluster);
+
+
+		
+	}
+
+	
+	// Update is called once per frame
+
+}
+[System.Serializable]
+public class Creature
+{
+	public int kluster;
+	public string[] koncept;
+	public int[] vektor;
+	public string rubrik;
+	public int annonsid;
+
+}
---
+++ Rymden/Assets/SceneChange.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChange : MonoBehaviour
+{
+	[SerializeField]
+	private string loadLevel;
+
+
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.tag == "Player") 
+		{
+			SceneManager.LoadScene (loadLevel);	
+		
+		
+		}
+	
+	
+	
+	
+	}
+	// Use this for initialization
+
+
+
+
+}
